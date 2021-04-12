65637231da5468a9998e77896280a71620ed6e9e
Sanket Pande <sanketpande99001@gmail.com>
2021-04-12 23:42:32 +0530

    Added print functionality and seperated the template from the main page

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-window.print();
+let printArea = document.getElementById("resume-container")
+
+printfunction = () => {
+    resumeIframe = printArea.contentWindow;
+    resumeIframe.focus()
+    resumeIframe.print()
+}
+
+document.getElementById("print-resume-btn").addEventListener("click", printfunction)
